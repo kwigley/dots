295036f76bf274135870fb2454616640b5336b1e
Kyle Wigley <kyle@fishtownanalytics.com>
2021-02-15 00:26:59 -0500

    Add private_dot_config/nvim/.keep Add private_dot_config/nvim/init.lua

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- Map leader to space
+vim.g.mapleader = ' '
+
+local fn = vim.fn
+local execute = vim.api.nvim_command
+
+-- Sensible defaults
+require('settings')
+
+-- Auto install packer.nvim if not exists
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+
+-- Install plugins
+require('plugins')
+
+-- Key mappings
+require('keymappings')
+
+-- Setup Lua language server using submodule
+require('lsp')
+
+-- Another option is to groups configuration in one folder
+require('config')
