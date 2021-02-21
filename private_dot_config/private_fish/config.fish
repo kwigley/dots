ba3b5fee32d62902fdfeab7ad305c1eacc88a6c3
Kyle Wigley <kyle@fishtownanalytics.com>
2021-02-21 15:59:31 -0500

    Update private_dot_config/alacritty/alacritty.yml Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,22 +1,24 @@
+# ENV vars
+set -gx EDITOR vi
+set -gx VISUAL nvim
+set PATH $PATH /Users/kwigley/.local/bin
+
 # Abbreviations/Aliases
 abbr --add senv 'source env/bin/activate.fish'
 abbr --add d 'deactivate'
 abbr --add vim 'nvim'
 abbr --add n 'nvim'
-abbr --add e 'code'
+abbr --add e 'nvim'
 abbr --add tm 'tmux -2 new-session -A -s main'
 abbr --add src 'source ~/.config/fish/config.fish'
 abbr --add ch 'chezmoi'
 abbr --add rd 'rm -r'
 abbr --add c 'z'
 
-# ENV vars
-set PATH $PATH /Users/kwigley/.local/bin
-
+# External Configs
 set -g fish_user_paths "/usr/local/opt/luajit-openresty/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
-# External Configs
 if test -f /usr/local/opt/asdf/asdf.fish
     source /usr/local/opt/asdf/asdf.fish
 end
+
