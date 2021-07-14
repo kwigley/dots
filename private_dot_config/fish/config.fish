62204500c39192d8699cb2851c08834efb9f02ad
Kyle Wigley <kwigley44@gmail.com>
2021-07-14 01:58:32 -0400

    add remaining lsp config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -5,25 +5,27 @@ set -gx PYTHONBREAKPOINT ipdb.set_trace
 set -gx GPG_TTY (tty)
 set -gx ASDF_DATA_DIR ~/.asdf
 set -gx DENO_INSTALL ~/.deno
+set -gx GOPATH $HOME/go
 
 # Abbr/Aliases
 abbr --add senv 'source env/bin/activate.fish'
-abbr --add d 'deactivate'
-abbr --add vim 'nvim'
-abbr --add n 'nvim'
+abbr --add d deactivate
+abbr --add vim nvim
+abbr --add n nvim
 abbr --add e $EDITOR
 abbr --add src 'source ~/.config/fish/config.fish'
-abbr --add ch 'chezmoi'
+abbr --add ch chezmoi
 abbr --add rd 'rm -r'
-abbr --add c 'z'
+abbr --add c z
 abbr --add dotfiles $EDITOR ~/.config
 
 # External Configs
-set -g fish_user_paths "/usr/local/opt/luajit-openresty/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths /usr/local/opt/luajit-openresty/bin $fish_user_paths
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
 set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 set -g fish_user_paths "$DENO_INSTALL/bin" $fish_user_paths
+set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
 if test -f /usr/local/opt/asdf/asdf.fish
     source /usr/local/opt/asdf/asdf.fish
 end
