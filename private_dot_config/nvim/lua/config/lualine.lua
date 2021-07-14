43e0d087bba33ffdc40ae0cbb3a1d000bda1c570
Kyle Wigley <kyle@fishtownanalytics.com>
2021-07-14 14:47:54 -0400

    fix file path

--- private_dot_config/nvim/lua/config/lualine.lua
+++ private_dot_config/nvim/lua/config/lualine.lua
@@ -31,7 +31,7 @@ local config = {
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch" },
-    lualine_c = { { "diagnostics", sources = { "nvim_lsp" } }, "filename" },
+    lualine_c = { { "diagnostics", sources = { "nvim_lsp" } }, { "filename", path = 1 } },
     lualine_x = { "filetype", lsp_progress },
     lualine_y = { "progress" },
     lualine_z = { clock },
