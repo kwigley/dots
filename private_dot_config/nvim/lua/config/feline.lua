dfca4b05de8d43da3fbcabcd3503660a0ec8f929
Kyle Wigley <kyle@fishtownanalytics.com>
2021-08-04 00:56:36 -0400

    update status line colors

--- private_dot_config/nvim/lua/config/feline.lua
+++ private_dot_config/nvim/lua/config/feline.lua
@@ -1,28 +1,29 @@
+local tokyonight = require("tokyonight.colors").setup()
+local colors = require("feline.defaults").colors
+
 local M = {}
 
 function M.load()
-	local tokyonight_colors = require("tokyonight.colors").setup()
-	local colors = require("feline.defaults").colors
 	local overrides = {
-		fg = tokyonight_colors.fg,
-		bg = tokyonight_colors.bg_statusline,
-		black = tokyonight_colors.black,
-		skyblue = tokyonight_colors.blue,
-		cyan = tokyonight_colors.cyan,
-		green = tokyonight_colors.green,
-		oceanblue = tokyonight_colors.blue0,
-		magenta = tokyonight_colors.magenta,
-		orange = tokyonight_colors.orange,
-		red = tokyonight_colors.red,
-		violet = tokyonight_colors.purple,
-		white = tokyonight_colors.fg,
-		yellow = tokyonight_colors.yellow,
+		fg = tokyonight.fg,
+		bg = tokyonight.bg_statusline,
+		black = tokyonight.black,
+		cyan = tokyonight.cyan,
+		green = tokyonight.green,
+		magenta = tokyonight.magenta,
+		orange = tokyonight.orange,
+		red = tokyonight.red,
+		yellow = tokyonight.yellow,
+		oceanblue = tokyonight.bg_visual,
+		skyblue = tokyonight.bg_search,
+		violet = tokyonight.purple,
 	}
 	for k, v in pairs(overrides) do
 		colors[k] = v
 	end
 	require("feline").setup({
 		colors = colors,
+		preset = "noicon",
 	})
 end
 
