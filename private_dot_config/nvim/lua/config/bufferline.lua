a371427d6f70c55cab4e412c600420136b82e07b
Kyle Wigley <kyle@fishtownanalytics.com>
2021-08-25 16:43:41 -0400

    vim: rm mappings setting from bufferline config

--- private_dot_config/nvim/lua/config/bufferline.lua
+++ private_dot_config/nvim/lua/config/bufferline.lua
@@ -1,15 +1,5 @@
 require("bufferline").setup({
   options = {
-    mappings = true,
     diagnostics = "nvim_lsp",
-    -- offsets = { { filetype = "NvimTree" } },
-    -- diagnostics_indicator = function(_, _, diagnostics_dict)
-    -- 	local s = " "
-    -- 	for e, n in pairs(diagnostics_dict) do
-    -- 		local sym = e == "error" and " " or (e == "warning" and " " or "")
-    -- 		s = s .. n .. sym
-    -- 	end
-    -- 	return s
-    -- end,
   },
 })
