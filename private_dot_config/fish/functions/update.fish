ed6c029cb8ccbcc3fbeade2bafd4d3cfc161eab4
Kyle Wigley <kyle@dbtlabs.com>
2021-11-22 12:39:24 -0500

    add lsp deps

--- private_dot_config/fish/functions/update.fish
+++ private_dot_config/fish/functions/update.fish
@@ -2,7 +2,23 @@ function update --description 'Update installed software'
     echo "update macos"
     softwareupdate -i -a
     echo "updating homebrew installed software"
-    chezmoi execute-template <~/.local/share/chezmoi/run_once_before_install-packages-darwin.sh.tmpl | sh
+    chezmoi execute-template <(chezmoi source-path)/run_once_before_install-packages-darwin.sh.tmpl | sh
+    brew upgrade
     echo "updating fisher plugins"
     fisher update
+    echo "updating rust deps"
+    cargo install stylua
+    echo "updating go deps"
+    go install golang.org/x/tools/gopls@latest
+    echo "updating python deps"
+    pip install -U -r ~/.default-python-packages
+    pipx ensurepath
+    pipx install pre-commit
+    pipx install tox
+    pipx install flake8
+    pipx install black
+    pipx upgrade-all
+    echo "updating node deps"
+    cat ~/.default-npm-packages | xargs npm i -g
+    npm update -g
 end
