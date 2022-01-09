8a620c67e58819316bf2eb973390b7e76b4fcf6c
Kyle Wigley <kwigley44@gmail.com>
2022-01-09 18:17:42 -0500

    rm CC var

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -6,7 +6,6 @@ set -gx GPG_TTY (tty)
 set -gx ASDF_DATA_DIR ~/.asdf
 set -gx DENO_INSTALL ~/.deno
 set -gx GOPATH $HOME/go
-set -gx CC GCC
 
 # Abbr/Aliases
 abbr --add senv 'source env/bin/activate.fish'
