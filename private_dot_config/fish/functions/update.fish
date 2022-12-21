66223941203d80c581a714561d111ed1cd16c25b
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2022-12-21 18:58:02 -0500

    update prompt

--- private_dot_config/fish/functions/update.fish
+++ private_dot_config/fish/functions/update.fish
@@ -17,7 +17,7 @@ function update --description 'Update installed software'
     fisher install jorgebucaran/autopair.fish
     fisher install lilyball/nix-env.fish
     fisher install oakninja/MakeMeFish
-    fisher install pure-fish/pure
+    fisher install IlanCosman/tide@v5
     fisher update
     echo "updating asdf"
     asdf plugin add nodejs
