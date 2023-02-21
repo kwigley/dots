5b95a38e7d2b0a0edbaf6ef803c16b279c1060fa
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-02-21 17:05:21 -0500

    fix brew

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -30,6 +30,7 @@ fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$GOPATH/bin"
 fish_add_path "$PNPM_HOME"
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
 source (brew --prefix asdf)/libexec/asdf.fish
 
 # Source fish config not tracked by git
