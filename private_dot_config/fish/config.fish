0a107540d4a7b84fe3344ffeb99a838408132329
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-02-21 16:56:33 -0500

    fix quotes

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -30,7 +30,7 @@ fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$GOPATH/bin"
 fish_add_path "$PNPM_HOME"
 
-source "(brew --prefix asdf)"/libexec/asdf.fish"
+source (brew --prefix asdf)/libexec/asdf.fish
 
 # Source fish config not tracked by git
 if test -f $HOME/.config/fish/local.fish
