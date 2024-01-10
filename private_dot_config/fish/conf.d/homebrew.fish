e80fb5ce66d69fd1a96c6d7e71a9dbdc9b935bea
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-01-10 13:52:37 -0500

    fix homebrew

--- private_dot_config/fish/conf.d/homebrew.fish
+++ private_dot_config/fish/conf.d/homebrew.fish
@@ -1,6 +1,14 @@
 # Thanks macOS
 if test (uname) = Darwin
-    eval (/opt/homebrew/bin/brew shellenv)
+    set -gx HOMEBREW_PREFIX /opt/homebrew
+    set -gx HOMEBREW_CELLAR /opt/homebrew/Cellar
+    set -gx HOMEBREW_REPOSITORY /opt/homebrew
+    ! set -q PATH; and set PATH ''
+    set -gxa PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+    ! set -q MANPATH; and set MANPATH ''
+    set -gxa MANPATH /opt/homebrew/share/man $MANPATH
+    ! set -q INFOPATH; and set INFOPATH ''
+    set -gxa INFOPATH /opt/homebrew/share/info $INFOPATH
     ulimit -n 10240
 end
 
